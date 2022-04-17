0525edb86cbc568fbb37d27eeb2d34cc09be432f
SanaHeydari667 <101060106+SanaHeydari667@users.noreply.github.com>
2022-04-17 22:33:35 +0430

    Create HW-W6-H Tower.py

---
+++ HW-W6-H Tower.py
@@ -0,0 +1,10 @@
+def TowersOfHanoi(n, s_tower, d_tower, h_tower):
+    if n == 1:
+        print("Move disc 1 from pole", s_tower, "to pole", d_tower)
+        return
+    TowersOfHanoi(n - 1, s_tower, h_tower, d_tower)
+    print("Move disc", n, "from pole", s_tower, "to pole", d_tower)
+    TowersOfHanoi(n - 1, h_tower, d_tower, s_tower)
+
+n = int(input("please input the number of disks: "))
+TowersOfHanoi(n, 'A', 'C', 'B')
